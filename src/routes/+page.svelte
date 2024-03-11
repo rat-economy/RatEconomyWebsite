5673e49a0ee6e446a33cf15ce3afdb58ceb83490
William Zhao <williezhao123@gmail.com>
2024-03-10 23:49:21 -0700

    Embedded RatPack Trailer to website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to Rat Economy!</h1>
 <p>Check out our <a href="https://rat-economy.itch.io">itch.io</a>!</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/BEUA2wNjCAA?si=HaF8VwE0ekJjD2Wy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
